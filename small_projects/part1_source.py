1df4d6174a61c99cafddb0c614c5b3a2cfb3ac6b
Vamsee" Krishna Reddy Narahari <vamsee.1608@gmail.com>
2023-02-08 02:49:52 -0800

    Update

--- small_projects/part1_source.py
+++ small_projects/part1_source.py
@@ -5,11 +5,11 @@ from tqdm import tqdm
 import time
 from fake_useragent import UserAgent
 import os
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
 headers = {'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36 Edg/88.0.705.56"}## Defining headers to avoid errors
 
 def main():
